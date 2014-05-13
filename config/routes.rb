a63886aade191bdfbf79c2d0b6263f8aed8e4cba
ngoduyson <ngo.duy.son@framgia.com>
2014-05-13 11:45:52 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Sample2App::Application.routes.draw do
   resources :users
-  get "users/new"
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
